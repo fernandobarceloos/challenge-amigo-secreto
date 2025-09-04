ab64c2620f96e968a6d66092ec12e76ec2c83e58
fernandobarceloos <fernandobarcelos.developer@gmail.com>
2025-09-04 14:38:44 -0300

    Função para atualizar a lista

--- app.js
+++ app.js
@@ -13,14 +13,12 @@ function agregarAmigo() {
     }
 
     // Validar que o nome é duplicado
-
     if(amigos.includes(nomeAmigo)) {
         alert(`Este nome ${nomeAmigo}já consta na lista, adicione um novo.`);
         return;
     }
 
     // Agrega o nome a lista de amigos
-
     amigos.push(nomeAmigo);
 
     // limpa o campo entrada
@@ -31,6 +29,19 @@ function agregarAmigo() {
 
 }
 
+// Função para atualizar a lista 
+    function atualizarLista() {
 
+        let listaAmigos = document.getElementById('listaAmigos');
+        // limpa a lista atual
+        listaAmigos.innerHTML = "";
 
+        //percorre o array
+        for(let i = 0; i < amigos.length; i++){
+            let list = document.createElement('list');
+            list.textContent = amigos[i];
+            listaAmigos.appendChild(list);
+        }
+
+    }
 
